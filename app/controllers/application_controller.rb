b9dc60ac4e9dcb18c0f7984582b44081454e9225
Sheng <niusheng@gmail.com>
2011-05-15 02:04:59 +1000

    add @ filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  helper_method :current_user, :online_users
+  helper_method :current_user, :online_users, :isAllowedToSee?
   
   private 
   def current_user
@@ -12,5 +12,28 @@ class ApplicationController < ActionController::Base
   def online_users
     @online_users = Onlineuser.all
   end
+  
+  private
+  def isAllowedToSee?(message,usr)
+    if !message.content.include?('@')
+      return true
+    end
+    if message.user == usr.nickname
+      return true
+    end
+    name = []
+    str = message.content+' '
+    while str.include?('@')
+      name2 = str[/@[a-zA-z]*[\s]/]
+      name2[' '] = ''
+      str["@"] = " "  
+      name <<name2 
+    end
+    if name.include?('@'+usr.nickname)
+      true
+    else
+      false
+    end
+  end  
   
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,50 @@
 $(function() {
   var faye = new Faye.Client('http://vivid-sword-660.heroku.com/bayeux');
   faye.subscribe("/messages/new", function(data) {
-    eval(data);
+    if(valuate(data,$('#username').text()))
+    {
+      eval(data);
+    }
   });
-  faye.subscribe("/sessions/new", function(data) {
-    eval(data);
-  });
+  $('#username').hide(); 
 });
 
+function valuate(data,user_name)
+{
+   i = data.indexOf("created_at")+17;
+   j = data.indexOf(' ',i);
+   creator_name = data.slice(i,j);  
+   if(creator_name == user_name)
+   {  
+      return true;
+   }
+   
+   i = data.indexOf("<li>")+4;
+   j = data.indexOf("<span");
+   msg = data.slice(i,j)+' '; 
+   while(msg.indexOf("\\n") != -1)
+   {   
+      msg = msg.replace("\\n",'');
+   }
+   
+   if(msg.indexOf('@') == -1)
+   {
+      return true;
+   }
+   else
+   {
+      while(msg.indexOf('@') != -1)
+      {      
+        i = msg.indexOf('@')+1;
+        j = msg.indexOf(' ',i);
+        mentioned_name = msg.slice(i,j);
+        if(mentioned_name == user_name)
+        {
+            return true;
+        }
+        msg = msg.substring(j);               
+      } 
+      return false;     
+   }      
+}
 
